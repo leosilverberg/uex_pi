f2ec4a5d1d1f2c77cb5415d6d22406eecac8056f
Leo Silverberg <leosilverberg@gmail.com>
2017-05-26 16:28:40 +0100

    change res again

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function startStreaming(io) {
     return;
   }
  
-  var args = ["-w", "1920", "-h", "1080", "-o", "./stream/image_stream.jpg", "-t", "999999999", "-tl", "70"];
+  var args = ["-w", "640 ", "-h", "480", "-o", "./stream/image_stream.jpg", "-t", "999999999", "-tl", "70"];
   proc = spawn('raspistill', args);
  
   console.log('Watching for changes...');
